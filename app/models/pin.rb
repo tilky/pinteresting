4e14318e44882d54ffc5d8403a8e40a235f2abe0
Evan Winter <ewinter1996@gmail.com>
2016-05-25 17:46:47 -0500

    changed aws

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/Desktop/white.png"
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
